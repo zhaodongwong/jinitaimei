244bd028c9434967c8acfa348199a8946a399291
zhaodongwong <sayhingchia1303@gmail.com>
2022-06-13 20:31:45 +0800

    hgj

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import ji from "./audioClips/ji.mp3";
 import ni from "./audioClips/ni.mp3";
 import tai from "./audioClips/tai.mp3";
 import mei from "./audioClips/mei.mp3";
+import niganma from "./audioClips/niganma.mp3";
+import haha from "./audioClips/haha.mp3";
+import yo from "./audioClips/yo.mp3";
 import {Howl, Howler } from "howler"; 
+import { Icon } from '@iconify/react';
 
 const audioClips = [
   {sound: ji, label:'鸡'},
-  {sound: ni, label:'你'},
-  {sound: tai, label:'太'},
-  {sound: mei, label:'美'},
+  {sound: ni, label:'||你'},
+  {sound: tai, label:'||太'},
+  {sound: mei, label:'||美'},
+  {sound: niganma, label:'||你干嘛'},
+  {sound: haha, label:'||哈哈'},
+  {sound: yo, label:' ||哟'},
 ]
 
 
@@ -32,8 +39,9 @@ class App extends Component {
 
   render() {
     Howler.volume(1.0)
-    return<div>
+    return<div className="text-5xl w-full h-screen  justify-center">
       {this.RenderButtonAndSound()}
+      <Icon icon="emojione-monotone:chicken" className="w-full h-1/2 text-black opacity-[20%] animate-spin [animation-duration:12s]"/>
     </div>;
   }
 }
