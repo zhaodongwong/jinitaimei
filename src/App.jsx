34efe81c31eae0a327a078bf0603c51c6b26c4c0
zhaodongwong <sayhingchia1303@gmail.com>
2022-06-16 16:50:20 +0800

    nice

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import ji from "./audioClips/ji.mp3";
 import ni from "./audioClips/ni.mp3";
 import tai from "./audioClips/tai.mp3";
 import mei from "./audioClips/mei.mp3";
-import niganma from "./audioClips/niganma1.mp3";
-import haha from "./audioClips/haha1.mp3";
-import yo from "./audioClips/yo1.mp3";
+import niganma from "./audioClips/niganma2.mp3";
+import haha from "./audioClips/haha2.mp3";
+import yo from "./audioClips/yo2.mp3";
 import {Howl, Howler } from "howler"; 
 import { Icon } from '@iconify/react';
 
 const audioClips = [
-  {sound: ji, label:'鸡'},
-  {sound: ni, label:'||你'},
-  {sound: tai, label:'||太'},
-  {sound: mei, label:'||美'},
-  {sound: niganma, label:'||你干嘛'},
-  {sound: haha, label:'||哈哈'},
-  {sound: yo, label:' ||哟'},
+  {sound: ji, label:'鸡|'},
+  {sound: ni, label:'|你|'},
+  {sound: tai, label:'|太|'},
+  {sound: mei, label:'|美|'},
+  {sound: niganma, label:'|你干嘛|'},
+  {sound: haha, label:'|哈哈|'},
+  {sound: yo, label:' |哟'},
 ]
 
 
